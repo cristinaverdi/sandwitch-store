1bb60fd91ccbb62c29c4891d3ae0b10d8cf3e39c
cverdi <cristina.verdi.tobon@gmail.com>
2018-06-19 17:27:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Fantastic Sandwitch Store
+--
+Best sandwitch store in town.
+
+###### Cristina Verdi @Codurance, June 2018 
---
+++ src/main/kotlin/com/codurance/sandwitchstore/sandwitchstore/SandwitchstoreApplication.kt
@@ -0,0 +1,11 @@
+package com.codurance.sandwitchstore.sandwitchstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SandwitchstoreApplication
+
+fun main(args: Array<String>) {
+    runApplication<SandwitchstoreApplication>(*args)
+}
